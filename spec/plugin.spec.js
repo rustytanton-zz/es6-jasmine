73d55b928d90d0951c017c92886eea4c9f909ddc
Rusty Tanton <rusty@perfectsensedigital.com>
2015-06-23 16:48:11 -0400

    removed unneded test from spec

--- spec/plugin.spec.js
+++ spec/plugin.spec.js
@@ -12,14 +12,11 @@ describe('plugin', () => {
 	});
 
 	it('should replace a test element with expected text', (done) => {
-		expect(true).toBe(true);
-		
 		new MainPlugin( $('#testContainer') );
 		setTimeout(() => {
 			expect( $('#testContainer').html() ).toBe('hello from ES6');
 			done();
 		}, 50);
-		
 	});
 
 });
